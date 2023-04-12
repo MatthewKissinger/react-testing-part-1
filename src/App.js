822b025dd5cb3e15547add8e4348287c0d4dcfe7
Matt <matthew.t.kissinger@gmail.com>
2023-04-12 15:07:39 -0700

    react testing library video tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { useState, useEffect } from 'react'
 import './App.css';
-import User from './User';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-      .then((res) => res.json())
-      .then((user) => setUser(user))
-      .catch((error) => setError(error.message));
-  })
-
-  if (error) {
-    return <span>{error}</span>
-  }
 
   return (
     <div>
-      {user ? <User user={user} /> : <span>Loading...</span>}
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,48 +1,4 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import App from './App';
 
-describe('Testing App Component', () => {
-  
-  test('loading text is shown while API request is in progress', async() => {
-    render(<App />)
-  
-    const loading = screen.getByText('Loading...');
-    expect(loading).toBeInTheDocument();
-  
-    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
-  })
-
-  test("user's name is rendered", async() => {
-    // Mocking the fetch function in App.js
-    window.fetch = jest.fn(() => {
-      const user = { name: 'Jack', email: 'jack@email.com' };
-
-      return Promise.resolve({
-        json: () => Promise.resolve(user),
-      });
-    });
-
-    render(<App />)
-  
-    const userName = await screen.findByText('Jack');
-    expect(userName).toBeInTheDocument();
-  })
-
-  test('error message is shown', async() => {
-    window.fetch.mockImplementationOnce(() => {
-      return Promise.reject({ message: 'API is down' });
-    });
-  
-    render(<App />);
-
-    window.fetch.mockImplementationOnce(() => {
-      return Promise.reject({ message: 'API is down' });
-    });
-  
-    const errorMessage = await screen.findByText('API is down');
-    expect(errorMessage).toBeInTheDocument();
-  })
-})
 
 
 
